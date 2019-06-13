e1e8b645032ad03012686bf41ab16640839c30d4
Manjula Mishra <manjula.mishra@gmail.com>
2019-06-13 09:27:18 -0700

    binary search working but DLL still have issues

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,44 +5,64 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if self.value:
-      if value < self.value:
-        if self.left is None:
-          self.left = BinarySearchTree(value)
-        else:
-          self.left.insert(value)
-      elif value > self.value:
-        if self.right is None:
-          self.right = BinarySearchTree(value)
-        else:
-          self.right.insert(value)
+    if value == self.value:
+      return "Value found"
+    if value < self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    elif value > self.value:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
     else:
       self.value = value
 
   def contains(self, target):
+    if target == self.value:
+      return "Value found"
     if target < self.value:
       if self.left is None:
-        return None, None
-      return self.left.contains(target, self)
+        return False
+      return self.left.contains(target)
     elif target > self.value:
       if self.right is None:
-        return None, None
-      return self.right.contains(target, self)
+        return False
+      return self.right.contains(target)
     
     
 
   def get_max(self):
-    if self.value == None:
-      return "Not found"
-    res = self.value
-    lres = self.left
-    rres = self.right
-    if lres > res:
-      res = lres
-    if rres > res:
-      res = rres
-    return res
-
+    if self.left is None and self.right is None:
+      return self.value
+    elif self.left is None:
+      rvalue = self.right.get_max()
+      if self.value > rvalue:
+        return self.value
+      else:
+        return rvalue
+    elif self.right is None:
+      lvalue = self.left.get_max()
+      if self.value > lvalue:
+        return self.value
+      else:
+        return lvalue
+    else:
+      lres = self.left.get_max()
+      rres = self.right.get_max()
+      res = self.value
+      if (lres > res):
+        res = lres
+      if (rres > res):
+        res = rres
+      return res
 
   def for_each(self, cb):
-    pass
+    print (self.value)
+    cb(self.value)
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -44,21 +44,23 @@ class DoublyLinkedList:
     return self.length
 
   def add_to_head(self, value):
+    self.length +=1
+    new_node = ListNode(value = value)
+    new_node.next = None
+    new_node.prev = None
     if self.head is None:
-      # allocate the Node and put in teh data
-      new_node = ListNode(value = value)
-
-      new_node.next = self.head
-      new_node.prev = None
-      if self.head is not None:
-        self.head.prev = new_node
       self.head = new_node
-
+      self.tail = new_node
+    else:
+      self.head.prev = new_node
+      new_node.next = self.head
+      self.head = new_node
     
   def remove_from_head(self):
     if self.head == None:
       return
     else:
+      self.length -=1
       if self.head != self.tail:
         self.head = self.head.next
         self.head.prev = None
@@ -66,18 +68,17 @@ class DoublyLinkedList:
         self.head = self.tail=None
 
   def add_to_tail(self, value):
+    self.length +=1
     new_node = ListNode(value=value)
     new_node.next = None
-    if self.head is None:
-      new_node.prev = None
+    new_node.prev = None
+    if self.tail is None:
       self.head = new_node
-      return
-    last = self.head
-    while (last.next is not None):
-      last = last.next
-      last.next = new_node
-      new_node.prev = last
-      return
+      self.tail = new_node
+    else:
+      self.tail.next = new_node
+      new_node.prev = self.tail
+      self.tail = new_node
 
    
   def remove_from_tail(self):
@@ -94,13 +95,51 @@ class DoublyLinkedList:
     
 
   def move_to_front(self, node):
-    pass
+    # get to the front node (there won't be a previous node)
+    curr_node = self
+    while curr_node.prev is not None:
+      print("moving to the prev node")
+      curr_node = curr_node.prev
+    # then add the node to its head
+    curr_node.add_to_head(node)
+   
 
   def move_to_end(self, node):
-    pass
+    curr_node = self
+    while curr_node.next is not None:
+      print("moving to the next node")
+      curr_node = curr_node.next
+    curr_node.add_to_tail(node)
+    
 
   def delete(self, node):
-    pass
+    if node.next is None and node.prev is None:
+      print("deleting the only node left")
+      self.head = None
+      self.tail = None
+      self.length = 0
+    elif node.next is None:
+      print("deleting the tail node")
+      self.tail = node.prev
+      self.length -= 1
+    elif node.prev is None:
+      print("deleting the head node")
+      self.head = node.next
+      self.length -= 1
+    else:
+      print("deleting a middle node")
+      node.prev.next = node.next
+      node.next.prev = node.prev
+
     
   def get_max(self):
-    pass
+    current = self.head
+    if (self.head == None):
+      return 0
+    else:
+      max = current.value
+      while (current.next != None):
+        current = current.next
+        if (current.value>max):
+          max = current.value
+    return max
